0caf833a13d6d0b4e169b9aac7f4a388c34b7c1d
Lorenz Stangier <98korbis@gmail.com>
2015-08-08 21:54:08 +0200

    Inital commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualADB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        String output = "";
+
+        public MainWindow()
+        {
+            startServer();
+            InitializeComponent();
+        }
+
+        private void startServer()
+        {
+            Process proc = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName="adb",
+                Arguments="start-server",
+                CreateNoWindow=true
+            };
+            proc.StartInfo = psi;
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private void reboot(object sender, RoutedEventArgs e)
+        {
+            output = "";
+
+            if ((bool)radSystem.IsChecked)
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = "adb.exe";
+                proc.StartInfo.Arguments = "reboot";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                output += proc.StandardError.ReadToEnd();
+                proc.WaitForExit();
+                System.Diagnostics.Debug.WriteLine(output);
+                if (output == "") { output = "Rebooted succsessfully!\n"; };
+                outputADB.Text = outputADB.Text + "adb>" + output;
+            }
+            else if ((bool)radRecovery.IsChecked)
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = "adb.exe";
+                proc.StartInfo.Arguments = "reboot recovery";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                output += proc.StandardError.ReadToEnd();
+                proc.WaitForExit();
+                System.Diagnostics.Debug.WriteLine(output);
+                if (output == "") { output = "Rebooted succsessfully!\n"; };
+                outputADB.Text = outputADB.Text + "adb>" + output;
+            }
+            else if ((bool)radBootloader.IsChecked)
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = "adb.exe";
+                proc.StartInfo.Arguments = "reboot bootloader";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                output += proc.StandardError.ReadToEnd();
+                proc.WaitForExit();
+                System.Diagnostics.Debug.WriteLine(output);
+                if (output == "") { output = "Rebooted succsessfully!\n"; };
+                outputADB.Text = outputADB.Text + "adb>" + output;
+            }
+        }
+
+        private void fbReboot(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "fastboot.exe";
+            proc.StartInfo.Arguments = "reboot";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+            if (!proc.WaitForExit(5000))
+            {
+                output = "No device attached!";
+                proc.Kill();
+            }
+            else
+            {
+                output = "Rebooting...";
+            }
+            System.Diagnostics.Debug.WriteLine(output);
+            outputADB.Text = outputADB.Text + "FB>" + output + "\n";
+        }
+    }
+}
